47504049eba435c22f54db531e69399c35450f09
Enigmatic Leutnant <lebedev2002danil@mail.ru>
2022-04-10 14:59:32 +0500

    Проработана сущность Student, + сделана основа для последующих репозиториев\сервисов, которые затем будут реализованы

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(["prefix" => 'student'], function () {
+    Route::get("/{id?}", [StudentController::class, "get"]);
+    Route::post("/add", [StudentController::class, "create"]);
+    Route::put("/update/{id}", [StudentController::class, "update"]);
+    Route::delete("/delete/{id}", [StudentController::class, "delete"]);
+});
+
